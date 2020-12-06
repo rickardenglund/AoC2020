991f3e8469bbca119a59826965442b67a463f879
Rickard Englund <rickard.englund@skf.com>
2020-12-06 11:05:10 +0100

    add go solution

---
+++ days/06/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/rickardenglund/AoC2020/days/06
+
+go 1.14
--- days/06/go/main_test.go
+++ days/06/go/main_test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_intersect(t *testing.T) {
+	type args struct {
+		group        map[int32]bool
+		personString string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[int32]bool
+	}{
+		{
+			args: args{
+				group: map[int32]bool{65: true},
+				personString: "ABC",
+			},
+			want: map[int32]bool{65: true},
+
+		},
+		{
+			args: args{
+				group: map[int32]bool{65: true, 66: true},
+				personString: "AC",
+			},
+			want: map[int32]bool{65: true},
+
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intersect(tt.args.group, tt.args.personString); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("intersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
