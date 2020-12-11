de03930998f00a6c08cd819e83b1e33fe99000eb
Rickard Englund <rickard.englund@skf.com>
2020-12-11 20:36:30 +0100

    solve p2

--- days/11/day.py
+++ days/11/day.py
@@ -1,35 +1,39 @@
 import re
 import puzzle
 from datetime import datetime
-from enum import Enum
 
 
 def main():
     print('### day 11 ###')
-    start1 = datetime.now()
-    p1res = part1()
-    stop1 = datetime.now()
-    print(f'p1: {p1res}')
+    # start1 = datetime.now()
+    # p1res = part1()
+    # stop1 = datetime.now()
+    # print(f'p1: {p1res}')
 
     start2 = datetime.now()
     p2res = part2()
     stop2 = datetime.now()
     print(f'p2: {p2res}')
 
-    print(f'p1 took: {stop1 - start1}\np2 took: {stop2 - start2}')
+    print(f'p2 took: {stop2 - start2}')
 
 
 def part1():
     state = get_input(puzzle.input)
-    return simulate(state, count_immediate_neighbours)
+    return simulate(state, count_immediate_neighbours, lambda current, neighbours: spot_outcome(current, neighbours, 4))
 
 
-def simulate(state, count_neighbours):
+def part2():
+    state = get_input(puzzle.input)
+    return simulate(state, count_distant_neighbours, lambda current, neighbours: spot_outcome(current, neighbours, 5))
+
+
+def simulate(state, count_neighbours, spot_outcome):
     previous_state = []
 
     while state != previous_state:
         previous_state = state
-        state = next_round(state, count_neighbours)
+        state = next_round(state, count_neighbours, spot_outcome)
 
     count = 0
     for line in state:
@@ -40,17 +44,25 @@ def simulate(state, count_neighbours):
     return count
 
 
-def next_round(state: list[str], count_neighbours) -> list[str]:
+def spot_outcome(current: str, neighbours: dict[str, int], occupation_limit: int) -> str:
+    if current == EMPTY and neighbours[OCCUPIED] == 0:
+        return OCCUPIED
+    elif current == OCCUPIED and neighbours[OCCUPIED] >= occupation_limit:
+        return EMPTY
+    else:
+        return current
+
+
+def next_round(state: list[str], count_neighbours, spot_outcome) -> list[str]:
     out = []
     for y in range(len(state)):
         out.append(state[y].copy())
 
         for x in range(len(state[y])):
             neighbours = count_neighbours(state, x, y)
-            if state[y][x] == EMPTY and neighbours[OCCUPIED] == 0:
-                out[y][x] = OCCUPIED
-            elif state[y][x] == OCCUPIED and neighbours[OCCUPIED] >= 4:
-                out[y][x] = EMPTY
+
+            out[y][x] = spot_outcome(state[y][x], neighbours)
+
     return out
 
 
@@ -81,18 +93,27 @@ def count_distant_neighbours(state, x, y) -> dict[str, int]:
 
     for dy in [-1, 0, 1]:
         for dx in [-1, 0, 1]:
-            if dy == dx == 0 or dy + y < 0 or dx + x < 0 or dy + y >= len(state) or dx + x >= len(state[0]):
+            if dx == dy == 0:
                 continue
 
-            p = state[dy + y][dx + x]
+            p = find_distant_seat(state, x, y, dx, dy)
+
             count[p] += 1
 
     return count
 
 
-def part2():
-    state = get_input(puzzle.input)
-    return simulate(state, count_distant_neighbours)
+def find_distant_seat(state, x, y, dx, dy):
+    cur_x = x + dx
+    cur_y = y + dy
+    while 0 <= cur_y < len(state) and 0 <= cur_x < len(state[0]):
+        if state[cur_y][cur_x] in [OCCUPIED, EMPTY]:
+            return state[cur_y][cur_x]
+
+        cur_x = cur_x + dx
+        cur_y = cur_y + dy
+
+    return FLOOR
 
 
 FLOOR = '.'
--- days/11/test_day.py
+++ days/11/test_day.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
 import unittest
-from day import part1, part2, get_input, next_round
+from day import part1, part2, get_input, next_round, count_immediate_neighbours, spot_outcome
 
 
 class Test(TestCase):
     def test_part1(self):
-        self.assertEqual(2, part1())
+        self.assertEqual(2344, part1())
 
     def test_part2(self):
-        self.assertEqual(2, part2())
+        self.assertEqual(2076, part2())
 
     def test_nex_round(self):
         before = get_input('''#.##.##.##
@@ -32,8 +32,8 @@ L.L.L..L..
 #.LLLLLL.L
 #.#LLLL.##''')
         print(after)
-        print(next_round(before))
-
+        print(next_round(before, count_immediate_neighbours,
+                         lambda current, neighbours: spot_outcome(current, neighbours, 4)))
 
 
 if __name__ == '__main__':
