076be4c95df437bfb789bf2b2a2060a2cc5d0426
Rickard Englund <rickard.englund@skf.com>
2020-12-13 18:04:15 +0100

    solve d13

--- days/13/day.py
+++ days/13/day.py
@@ -1,4 +1,3 @@
-import re
 import puzzle
 from datetime import datetime
 
@@ -22,7 +21,6 @@ def part1(input: str) -> int:
     ticket, ids = get_input(input)
     wait_times = [(id, (id * (ticket // id) + id) % ticket) for id in ids]
     wait_times = sorted(wait_times, key=lambda tuple: tuple[1])
-    # print(wait_times)
 
     return wait_times[0][0] * wait_times[0][1]
 
@@ -37,28 +35,22 @@ def part2(input: str) -> int:
     return find_earliest_match(input.split('\n')[1])
 
 
-def find_earliest_match(input:str) -> int:
+def find_earliest_match(input: str) -> int:
     rules = get_rules(input)
-    rules = sorted(rules, reverse=True, key=lambda tuple: tuple[1])
-    print(rules)
-    print('step', rules[0][1])
 
-    t = rules[0][1] - rules[0][0]
-    if t == 0:
-        t+=rules[0][1]
-    while not is_valid(rules, t):
-        if t % 1_000_000 == 0:
-            print('checking', t, format(t, '2E'))
-        t += rules[0][1]
+    if not all((is_prime(id) for (offset, id) in rules)):
+        raise Exception('not all primes')
+
+    t = 0
+    step_size = 1
+    for (offset, id) in rules:
+        while not ((t + offset) % id == 0):
+            t += step_size
+
+        step_size *= id
+        # print(f't: {t}, step_size: {step_size}, {(offset, id)}')
 
     return t
-
-
-def is_valid(rules, t):
-    for (i, id) in rules:
-        if (t + i) % id != 0:
-            return False
-    return True
 
 
 def get_rules(input):
@@ -69,6 +61,20 @@ def get_rules(input):
             continue
         rules.append((i, int(ids[i])))
     return rules
+
+
+def is_prime(num):
+    if num == 1:
+        return True
+    if num > 1:
+        for i in range(2, num//2 + 1):
+            if (num % i) == 0:
+                return False
+
+        return True
+    else:
+        return False
+
 
 
 if __name__ == "__main__":
--- days/13/test_day.py
+++ days/13/test_day.py
@@ -1,20 +1,15 @@
 from unittest import TestCase
 import unittest
-from day import part1, part2, is_valid, get_rules, find_earliest_match
+from day import part1, part2, get_rules, find_earliest_match, is_prime
 import puzzle
 
 
 class Test(TestCase):
     def test_part1(self):
-        self.assertEqual(1, part1(puzzle.input))
+        self.assertEqual(171, part1(puzzle.input))
 
     def test_part2(self):
-        self.assertEqual(2, part2(puzzle.input))
-
-    def test_is_valid(self):
-        rules = get_rules(puzzle.test_input.split('\n')[1])
-        self.assertFalse(is_valid(rules, 545))
-        self.assertTrue(is_valid(rules, 1068781))
+        self.assertEqual(539746751134958, part2(puzzle.input))
 
     def test_earliest_match(self):
         self.assertEqual(754018, find_earliest_match('67,7,59,61'))
@@ -22,6 +17,14 @@ class Test(TestCase):
         self.assertEqual(779210, find_earliest_match('67,x,7,59,61'))
         self.assertEqual(1261476, find_earliest_match('67,7,x,59,61'))
         self.assertEqual(1202161486, find_earliest_match('1789,37,47,1889'))
+
+    def test_is_prime(self):
+        self.assertTrue(is_prime(1))
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(13))
+        self.assertTrue(is_prime(43))
+        self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(55))
 
 
 if __name__ == '__main__':
