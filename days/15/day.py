fc4568b9893d93a3cc3cc5cc139221d61e01d0fe
Rickard Englund <rickard.englund@skf.com>
2020-12-15 19:19:32 +0100

    some optimization d15

--- days/15/day.py
+++ days/15/day.py
@@ -1,4 +1,7 @@
 import re
+
+from typing import Dict
+
 import puzzle
 from datetime import datetime
 
@@ -27,30 +30,53 @@ def part2(input: str) -> int:
     return play_game(input, 30_000_000)
 
 
-def play_game(seed, target_round):
-    state = {}
-    for i in range(len(seed)):
-        state[seed[i]] = [i + 1]
-    prev_num = seed[len(seed) - 1]
+class History():
+    prev = None
+    prev_prev = None
 
+    def add(self, n):
+        self.prev_prev = self.prev
+        self.prev = n
+
+    def get_age(self):
+        if self.prev_prev is None:
+            return 0
+        return self.prev - self.prev_prev
+
+    def __str__(self):
+        return f'{self.prev},{self.prev_prev}'
+
+
+def play_game(seed, target_round):
+    state = initial_state(seed)
+
+    prev_num = seed[len(seed) - 1]
+    h_old = state.get(prev_num, History())
     current_round = len(seed) + 1
     while current_round <= target_round:
-        if len(state.get(prev_num, [])) <= 1:
-            new_num = 0
-        else:
-            l = state[prev_num][-2:]
-            new_num = l[1] - l[0]
+        new_num = h_old.get_age()
+        h_new = state.get(new_num, History())
+        h_new.add(current_round)
 
-        state[new_num] = new_history(state, current_round, new_num)
+        state[new_num] = h_new
 
         prev_num = new_num
-
-        # print(f'{current_round}: {prev_num}')
+        h_old = h_new
         current_round += 1
-        if current_round % 100_000 == 0:
-            print(f'round:{current_round}/{target_round}')
 
     return prev_num
+
+
+def initial_state(seed)-> Dict[int, History]:
+    state: Dict[int, History] = {}
+    for i in range(len(seed)):
+        if seed[i] in state.keys():
+            state[seed[i]].add(i + 1)
+        else:
+            h = History()
+            h.add(i + 1)
+            state[seed[i]] = h
+    return state
 
 
 def new_history(state, current_round, num):
--- days/15/test_day.py
+++ days/15/test_day.py
@@ -8,6 +8,9 @@ class Test(TestCase):
     def test_part1(self):
         self.assertEqual(595, part1(puzzle.input))
 
+    def test_test_part1(self):
+        self.assertEqual(436, part1(puzzle.test_input))
+
     def test_part2(self):
         self.assertEqual(1708310, part2(puzzle.input))
 
