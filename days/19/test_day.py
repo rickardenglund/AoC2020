503d995073040968f4670ff506f3281e007c716e
Rickard Englund <rickard.englund@skf.com>
2020-12-21 10:05:32 +0100

    fix linting

--- days/19/test_day.py
+++ days/19/test_day.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import unittest
-from day import part1, part2, matches, get_input
+from day import part1, part2
 import puzzle
 
 
@@ -21,33 +21,33 @@ class Test(TestCase):
     def test_part2(self):
         self.assertEqual(2, part2(puzzle.input))
 
-    def test_starts_with_simple(self):
-        rules, messages = get_input(puzzle.test_simple)
+    # def test_starts_with_simple(self):
+    #     rules, messages = get_input(puzzle.test_simple)
+    #
+    #     s, match = starts_with(rules, 1, 'apa')
+    #     self.assertTrue(match)
+    #
+    #     s, match = starts_with(rules, 1, 'bpa')
+    #     self.assertFalse(match)
+    #
+    #     s, match = starts_with(rules, 0, 'aba')
+    #     self.assertTrue(match)
+    #     self.assertEqual(s, 'aba')
+    #
+    #     s, match = starts_with(rules, 0, 'abb')
+    #     self.assertFalse(match)
+    #     self.assertEqual(s, '')
 
-        s, match = starts_with(rules, 1, 'apa')
-        self.assertTrue(match)
-
-        s, match = starts_with(rules, 1, 'bpa')
-        self.assertFalse(match)
-
-        s, match = starts_with(rules, 0, 'aba')
-        self.assertTrue(match)
-        self.assertEqual(s, 'aba')
-
-        s, match = starts_with(rules, 0, 'abb')
-        self.assertFalse(match)
-        self.assertEqual(s, '')
-
-    def test_starts_with_small(self):
-        rules, messages = get_input(puzzle.test_small)
-
-        s, match = starts_with(rules, 0, 'aba')
-        self.assertTrue(match)
-        self.assertEqual(s, 'aba')
-
-        s, match = starts_with(rules, 0, 'aab')
-        self.assertTrue(match)
-        self.assertEqual(s, 'aab')
+    # def test_starts_with_small(self):
+    #     rules, messages = get_input(puzzle.test_small)
+    #
+    #     s, match = starts_with(rules, 0, 'aba')
+    #     self.assertTrue(match)
+    #     self.assertEqual(s, 'aba')
+    #
+    #     s, match = starts_with(rules, 0, 'aab')
+    #     self.assertTrue(match)
+    #     self.assertEqual(s, 'aab')
 
     def test_test_part1_input_2(self):
         self.assertEqual(3, part1(puzzle.test_input_2))
